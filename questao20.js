bd5a4e540558b0e32fc58e7d3b95fef7c646c8b0
luizferrazz <117530933+luizferrazz@users.noreply.github.com>
2022-11-14 14:38:21 -0300

    Create questao20.js

---
+++ questao20.js
@@ -0,0 +1,5 @@
+/*Criar uma função que receba um array de números e retorne o menor
+número deste array */
+const nums = [55, 420, 710, 69, 33, -5, 0, -32]
+minimumValue = Math.min(...nums)
+console.log(minimumValue)
