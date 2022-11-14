2d643526351bee9c36b7ead08626cbf002b453b8
luizferrazz <117530933+luizferrazz@users.noreply.github.com>
2022-11-14 14:37:53 -0300

    Create questao19.js

---
+++ questao19.js
@@ -0,0 +1,5 @@
+/*Faça uma função que receba a base a altura de um triângulo e retorne
+a área desse triângulo. A precisão deverá ser de duas casas decimais,
+arredondando se necessário */
+anyTriangle = (base, height) =>(base * height)  / 2
+console.log(anyTriangle(20, 15)) 
