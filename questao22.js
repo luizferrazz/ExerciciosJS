89b49fbb49e8cb2ada565ebaef9ec8216b691501
luizferrazz <117530933+luizferrazz@users.noreply.github.com>
2022-11-17 14:21:23 -0300

    Create questao22.js

---
+++ questao22.js
@@ -0,0 +1,4 @@
+/*Criar uma função que receba uma string como parâmetro e conte quantas palavras tem nela */
+const quote = `Those who forgive themselves, and are able to accept their true nature... They are the strong ones`
+const amountOfWords = quote.split(' ').length
+console.log(amountOfWords)
